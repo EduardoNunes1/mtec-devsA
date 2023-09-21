9c53faaba89a1226c6c90f2ed383612015ae81da
Eduardo Nunes <eduardonunes8349@gmail.com>
2023-09-21 12:30:01 -0300

    Configuração de serviço e criação do link com o banco de dados

--- MtecDevs/Data/AppDbContext.cs
+++ MtecDevs/Data/AppDbContext.cs
@@ -13,4 +13,8 @@ public class AppDbContext : IdentityDbContext
     public DbSet<Usuario> Usuarios { get; set; }
     public DbSet<TipoDev> TipoDevs { get; set; }
 
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+    }
 }
